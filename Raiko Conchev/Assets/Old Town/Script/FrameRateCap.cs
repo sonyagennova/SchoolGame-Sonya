dbb4087b7f48be8898b612d995b1d37d65bfcbce
Sonya Genova <sonyagennova@gmail.com>
2025-06-24 08:38:00 +0300

    Script folder of OldTown of Assets

---
+++ Raiko Conchev/Assets/Old Town/Script/FrameRateCap.cs
@@ -0,0 +1,18 @@
+﻿//This is a targetfps script. Copyrights © of PolyPixel3D
+
+using UnityEngine;
+using System.Collections;
+
+public class FrameRateCap : MonoBehaviour {
+
+    //Set targetfps variable
+    public int TargetFPS = 60;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Sets target fps based of public int variable
+        Application.targetFrameRate = TargetFPS;
+
+    }
+}
